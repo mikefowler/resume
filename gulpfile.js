32380388423aff1d3126f5f5a7e39a479c04cc38
Mike Fowler <mike@mikefowler.me>
2014-06-08 19:58:02 -0700

    Update build tasks, update some styles.

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,29 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 
 // -----------------------------------------------------------------------------
 // Tasks
 // -----------------------------------------------------------------------------
 
+gulp.task('connect', function () {
+	connect.server({
+		port: 3700,
+		livereload: true
+	});
+});
+
 gulp.task('styles', function () {
 	gulp
 		.src('style.scss')
 		.pipe(sass({ outputStyle: 'compressed' }))
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest('./'))
+		.pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
 	gulp.watch('*.scss', ['styles']);
 });
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('default', ['styles', 'connect', 'watch']);
